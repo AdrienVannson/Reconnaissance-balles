5296abca690f160fb73f28d036e55922c8359b3e
Adrien VANNSON <adrien.vannson@protonmail.com>
2019-04-03 11:44:17 +0200

    Optimisations lors de la compilation

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-g++ -Wall -Wextra -g main.cpp Image/Image.cpp Image/Pixel.cpp  `pkg-config --libs opencv` -o prog
+g++ -Wall -Wextra -g -Ofast main.cpp Image/Image.cpp Image/Pixel.cpp  `pkg-config --libs opencv` -o prog
